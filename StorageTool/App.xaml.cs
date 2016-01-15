ba902133a12d74dd66d09189afabbc82026424e1
nhrjr <jester.raced@gmail.com>
2016-01-15 17:05:29 +0100

    Updated View

--- StorageTool/App.xaml.cs
+++ StorageTool/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace StorageTool
 {
@@ -15,17 +17,43 @@ namespace StorageTool
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.PreviewMouseLeftButtonDownEvent,
+               new MouseButtonEventHandler(SelectivelyHandleMouseButton), true);
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.GotKeyboardFocusEvent,
+              new RoutedEventHandler(SelectAllText), true);
 
-                //if (StorageTool.Properties.Settings.Default.Config == null)
-                //{
-                //    StorageTool.Properties.Settings.Default.Config = new Config();
-                //}
-            
+            //base.OnStartup(e);
+            //if (StorageTool.Properties.Settings.Default.Config == null)
+            //{
+            //    StorageTool.Properties.Settings.Default.Config = new Config();
+            //}
+
+
 
         }
         private void Application_Exit(object sender, ExitEventArgs e)
         {
             //StorageTool.Properties.Settings.Default.Save();
+        }
+
+        private static void SelectivelyHandleMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            var textbox = (sender as TextBox);
+            if (textbox != null && !textbox.IsKeyboardFocusWithin)
+            {
+                if (e.OriginalSource.GetType().Name == "TextBoxView")
+                {
+                    e.Handled = true;
+                    textbox.Focus();
+                }
+            }
+        }
+
+        private static void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+                textBox.SelectAll();
         }
     }
 }
